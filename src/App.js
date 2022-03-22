221ef1e338b8e52b5f61977fca02b1a622c0758e
Vlad Solokha <vsolokha@gmail.com>
2022-03-22 09:56:34 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { Octokit } from '@octokit/core'
 import './App.css';
 import './components/Search.css'
 import Result from './components/Result'
 
 
-export default function App() {
-  const octokit = new Octokit()
+export default function App() {  
   const [searchText, setSearchText] = useState('')
-  const [url, setUrl] = useState('')
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
   const [resultList, setResultList] = useState([])
-  const [sortUsers, setSortUsers] = useState('best match')
+  const [sortUsers, setSortUsers] = useState('best%20match')
   const [page, setPage] = useState(1)
+  const per_page = 10
+  const [url, setUrl] = useState('')
 
   const totalCount = resultList.total_count
+  const totalPages = totalCount/10;
 
-  
+  const refreshPage = () => {
+    window.location.reload()
+  }
+
+  const octokit = new Octokit({})
+  const request = octokit.request("GET /search/users", {
+    q: `${searchText}`,
+    sort: `${sortUsers}`,
+    per_page: `${per_page}`,
+    page: `${page}`
+  });
+
+
+
+
+
+  const onPageChange = () => {
+
+  }
+  useMemo(() => {
+  }, [])
+
   useEffect(() => {
     async function fetchHandle() {
-      setIsLoading(true)  
+      setIsLoading(true)
+
       try {
-        const result = await octokit.request("GET /search/users", {
-          q: `${searchText}`,
-          sortUsers: `${sortUsers}`,
-          per_page: 10,
-          page: `${page}`
-        });
+        const result = await {request}
         // const result = await axios.get(url)
         setResultList(result.data)
       } catch (error) {
@@ -35,8 +53,9 @@ export default function App() {
       }
       setIsLoading(false)
     }
-    (searchText !== '') && fetchHandle()
-  }, [url, sortUsers])
+    fetchHandle()
+  }, [])
+
 
   useEffect(() => {
     console.log(`searchText is: ${searchText}`)
@@ -48,38 +67,39 @@ export default function App() {
   
   return (
     <div className="App">
-      <header className="App-header" onClick='#'>
+      <header className="App-header" onClick={refreshPage}>
        GitHub User Search 
       </header>
 
       <div className='user-inputs'>
         {/* This Form can be placed in another component */}
-        <form name='search' onSubmit={ (e) => {
-            e.preventDefault()
-            setUrl(`https://api.github.com/search/users?q=${searchText}`)
-          }}>
-          <input 
-            className='search-bar'
-            name='search'
-            type='search'
-            placeholder='search for user'
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)} 
-          />        
-          <button
-            className='search-button'
-            type='submit'
-          >Search
-          </button>
-        </form>
- 
+        <div className='form-container'>
+          <form name='search' onSubmit={ (e) => {
+              e.preventDefault()
+              setUrl(`https://api.github.com/search/users?q=${searchText}&sort=${sortUsers}&per_page=${per_page}&page=${page}`)
+            }}>
+            <input
+              className='search-bar'
+              name='search'
+              type='search'
+              placeholder='search...'
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)} 
+            />
+            <button
+              className='search-button'
+              type='submit'
+            >Search
+            </button>
+          </form>
+          </div>
         <div className='filter-bar'>
           <label for="sort by">Sort by: </label>
             <select 
               name='sort' 
               onChange={(e) => {setSortUsers(e.target.value)}}
             >
-              <option value='best match'>Best Match</option>
+              <option value='best%20match'>Best Match</option>
               <option value='followers'>Followers</option>
               <option value='repositories'>Repositories</option>
               <option value='joined'>Joined Date</option>
@@ -94,7 +114,6 @@ export default function App() {
         </div>
       
         <div className='pagination'>
-          Display Pages
         </div>
       </div>
       {/* <Error error={error} /> */}
